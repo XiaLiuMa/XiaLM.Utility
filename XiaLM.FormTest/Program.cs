f3a268dacfa62d80e70b03905824adad85d0d57c
XiaLiuMa <am6x45@126.com>
2018-10-26 18:12:13 +0800

    u

--- XiaLM.FormTest/Program.cs
+++ XiaLM.FormTest/Program.cs
@@ -12,7 +12,7 @@ namespace XiaLM.FormTest
         [STAThread]
         static void Main()
         {
-            LogOutPut.GetInstance().Init("127.0.0.1", 12345);
+            LogOutPut.GetInstance().Init("127.0.0.1", 51888);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
